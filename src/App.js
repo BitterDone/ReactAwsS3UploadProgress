70942ea250480d8086c4ea0fefdc4c1e04a407cc
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-04-03 12:51:42 -0500

    Works as expected thanks to u/ScottRatigan

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AWS from 'aws-sdk';
+import { accessKeyId, secretAccessKey } from './secrets'
 
 function App() {
   const [selectedFiles, setSelectedFiles] = useState({})
@@ -9,14 +10,27 @@ function App() {
   
   var s3 = new AWS.S3({
     apiVersion: '2012-10-17',
-    accessKeyId: 'accessKeyId',
-    secretAccessKey: 'secretAccessKey',
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
   });
 
    const handleFile = event => {
       setSelectedFiles(event?.target?.files)
       setUploadProg(new Array(Object.keys(event?.target?.files).length).fill(0))
    }
+   
+   /*
+   https://stackoverflow.com/questions/61004673/the-callback-in-s3-putobject-onhttpuploadprogress-callback-doesnt-update
+   https://www.reddit.com/r/reactjs/comments/fu87ap/the_callback_in_s3putobjectonhttpuploadprogress/
+
+   const [arr, setArr] = useState([0, 0]);
+   const fn = () => {
+     setArr(arr => {
+       arr[0] = 1;
+       return arr;
+     });
+   }
+   */
 
    const upload = event => {
      Object.keys(selectedFiles).forEach((key, index) => {
@@ -25,18 +39,21 @@ function App() {
       var params = {
         Body: selectedFiles[key], 
         Bucket: "uploadprogress", 
-        Key: `exampleobject${index}`, 
+        Key: `exampleobject-_${index}`, 
        };
 
        s3.putObject(params)
         .on('httpUploadProgress', (progressEvent, response) => {
-          const newUploadProg = [...uploadProg]
-          const percent = parseInt(100*progressEvent.loaded / progressEvent.total)
-          newUploadProg[index] = percent
-          console.log(uploadProg)
-          console.log(newUploadProg)
-          console.log('______________')
-          setUploadProg(newUploadProg)
+          
+          setUploadProg(uploadProg =>{
+            const newUploadProg = [...uploadProg]
+            const percent = parseInt(100*progressEvent.loaded / progressEvent.total)
+            newUploadProg[index] = percent
+            console.log(uploadProg)
+            console.log(newUploadProg)
+            console.log('______________')
+            return newUploadProg
+          })
         })
         .send((err, data) => {
           if (err) console.log(err, err.stack);
@@ -45,22 +62,23 @@ function App() {
     })  
    }
 
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <input type="file" name="file" onChange={handleFile} multiple/>
-        <button type="button" class="btn btn-success btn-block" onClick={upload}>Upload</button>
-        {uploadProg.map(percent => (<div style={{
+        <button type="button" className="btn btn-success btn-block" onClick={upload}>Upload</button>
+        {uploadProg.map((percent, idx) => (<div key={idx} style={{
           margin: 5,
           width: 100,
           height: 10,
-          backgroundColor: 'green'
+          backgroundColor: 'yellow'
         }}>
           <div style={{
             height: 8,
             width: percent,
-            backgroundColor: 'yellow',
+            backgroundColor: 'green' ,
           }}>
           </div>
         </div>))}
